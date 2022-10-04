a76cc7f7c3bf04a4e53f8a499725d65a031c847b
tracer-ics2021 <tracer@njuics.org>
2022-10-04 01:48:32 -0700

    >  compile 210810119    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: c6b7f2de3c1940e6ab30986c0a49acc5            Boot ID: 37cfaf1f1a924e16968c4b98c949ffe2     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-48-generic       Architecture: x86-64  01:48:32 up 1 day,  5:45,  1 user,  load average: 0.29, 0.33, 0.26

--- nemu/src/isa/riscv32/instr/control.h
+++ nemu/src/isa/riscv32/instr/control.h
@@ -17,27 +17,27 @@ def_EHelper(jalr) {
 }
 
 def_EHelper(beq) {
-    rtl_jrelop(s, RELOP_EQ, dsrc1, dsrc2, id_dest->imm);
+    rtl_jrelop(s, RELOP_EQ, dsrc1, dsrc2, cpu.pc + id_dest->imm);
 }
 
 def_EHelper(bne) {
-    rtl_jrelop(s, RELOP_NE, dsrc1, dsrc2, id_dest->imm);
+    rtl_jrelop(s, RELOP_NE, dsrc1, dsrc2, cpu.pc + id_dest->imm);
 }
 
 def_EHelper(blt) {
-    rtl_jrelop(s, RELOP_LT, dsrc1, dsrc2, id_dest->imm);
+    rtl_jrelop(s, RELOP_LT, dsrc1, dsrc2, cpu.pc + id_dest->imm);
 }
 
 def_EHelper(bge) {
-    rtl_jrelop(s, RELOP_GE, dsrc1, dsrc2, id_dest->imm);
+    rtl_jrelop(s, RELOP_GE, dsrc1, dsrc2, cpu.pc + id_dest->imm);
 }
 
 def_EHelper(bltu) {
-    rtl_jrelop(s, RELOP_LTU, dsrc1, dsrc2, id_dest->imm);
+    rtl_jrelop(s, RELOP_LTU, dsrc1, dsrc2, cpu.pc + id_dest->imm);
 }
 
 def_EHelper(bgeu) {
-    rtl_jrelop(s, RELOP_GEU, dsrc1, dsrc2, id_dest->imm);
+    rtl_jrelop(s, RELOP_GEU, dsrc1, dsrc2, cpu.pc + id_dest->imm);
 }
 
 
