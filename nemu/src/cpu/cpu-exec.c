55b10b4188b2820b23dae09369f055684021ab20
tracer-ics2021 <tracer@njuics.org>
2022-10-05 08:56:52 -0700

    >  compile 210810119    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: c6b7f2de3c1940e6ab30986c0a49acc5            Boot ID: 37cfaf1f1a924e16968c4b98c949ffe2     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-48-generic       Architecture: x86-64  08:56:52 up 2 days, 59 min,  1 user,  load average: 0.30, 0.31, 0.25

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -121,7 +121,7 @@ void fetch_decode(Decode *s, vaddr_t pc) {
   void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
   disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.instr.val, ilen);
-
+printf("wtf");
   //在指令环形缓冲区添加指令(在执行指令之前)
   add_to_irbuf(s);
 
