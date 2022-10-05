f3d5d6f7e37deff69c8616ab746b4d5f0f1ae4c5
tracer-ics2021 <tracer@njuics.org>
2022-10-04 23:59:04 -0700

    >  compile 210810119    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: c6b7f2de3c1940e6ab30986c0a49acc5            Boot ID: 37cfaf1f1a924e16968c4b98c949ffe2     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-48-generic       Architecture: x86-64  23:59:04 up 1 day, 18:36,  1 user,  load average: 0.27, 0.23, 0.19

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -4,17 +4,81 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-
-int printf(const char *fmt, ...) {
-  panic("Not implemented");
+//数字转为字符串
+static int dig2str(char *str, int dig) {
+    assert(str != NULL);
+    char  buf[32];
+    char *p = buf;
+    if(dig == 0) *p++ = 0;
+    else {
+        while(dig / 10) {
+            *p++ = (dig % 10) + '0';
+            dig /= 10;
+        }
+        *p++ = (dig % 10) + '0';
+    }
+    *p = '\0';
+    int len = strlen(buf);
+    assert(len > 0);
+    while(len--) {
+        *str++ = buf[len];
+    }
+    return len;
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
+
+
+int printf(const char *fmt, ...) {
+    assert(fmt != NULL);
   panic("Not implemented");
+}
+//返回值为写入的字符数
+int vsprintf(char *out, const char *fmt, va_list ap) {
+    int ret = 0;
+    int flag = 0;
+    int arg_int;
+    const char *arg_str;
+    while(*fmt != '\0') {
+        if(flag == 0) {
+            if(*fmt == '%') flag = 1;
+            else *out = *fmt;
+            ret++;
+        }
+        else {
+            flag = 0;
+            switch(*fmt) {
+                case 'd':
+                    arg_int = va_arg(ap, int);
+                    ret += dig2str(out, arg_int);
+                    break;
+                case 's':
+                    arg_str = va_arg(ap, char *);
+                    while(*arg_str != '\0') {
+                        *out = *arg_str;
+                        out++;
+                        arg_str++;
+                    }//不加空字符
+                    ret += strlen(arg_str);
+                    break;
+                default: break;
+            }
+        }
+        out++;
+        fmt++;
+    }
+    return ret;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+    assert(out != NULL && fmt != NULL);
+    va_list ap;
+    int ret;
+
+    va_start(ap, fmt);
+    ret = vsprintf(out, fmt, ap);
+    va_end(ap);
+
+    return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
