d57ce629a7973743307a90b1e896f68e94ad4e9e
tracer-ics2021 <tracer@njuics.org>
2022-10-05 08:53:14 -0700

    >  compile 210810119    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: c6b7f2de3c1940e6ab30986c0a49acc5            Boot ID: 37cfaf1f1a924e16968c4b98c949ffe2     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-48-generic       Architecture: x86-64  08:53:14 up 2 days, 55 min,  1 user,  load average: 0.40, 0.28, 0.23

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -142,7 +142,7 @@ void cpu_exec(uint64_t n) {
   uint64_t timer_start = get_time();
 
   Decode s;
-  for (;n > 0; n --) {
+  for (;n > 0; n --) {  if(n==2) TODO();
     fetch_decode_exec_updatepc(&s);
     g_nr_guest_instr ++;
     trace_and_difftest(&s, cpu.pc);
