843caec86f13a309b125a0e231fee0c034c053e3
tracer-ics2021 <tracer@njuics.org>
2022-10-08 04:26:31 -0700

    >  compile 210810119    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: c6b7f2de3c1940e6ab30986c0a49acc5            Boot ID: cc1b675076c44f709fc39742a2b5d6b8     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-48-generic       Architecture: x86-64  04:26:31 up 18:37,  1 user,  load average: 0.24, 0.21, 0.19

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -3,11 +3,19 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-    if(pc != cpu.pc) return false;
-    for(int i = 0; i < 32; ++i) {
-        if(ref_r->gpr[i]._32 != cpu.gpr[i]._32) return false;
+    bool flag = true;
+    if(pc != cpu.pc) {
+        flag = false;
+        printf("pc fault!   spike: 0x%x nemu: 0x%x\n",pc,cpu.pc);
     }
-    return true;
+    for(int i = 0; i < 32; ++i) {
+        if(ref_r->gpr[i]._32 != cpu.gpr[i]._32) {
+            flag = false;
+            printf("reg%-2d fault! spike: %u nemu: %u\n",i,ref_r->gpr[i]._32, cpu.gpr[i]._32);
+        }
+
+    }
+    return flag;
 }
 
 void isa_difftest_attach() {
