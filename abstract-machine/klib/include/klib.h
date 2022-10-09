f3dc7451c129e923cfaa55062ad32625562715f6
tracer-ics2021 <tracer@njuics.org>
2022-10-08 18:55:28 -0700

    >  compile 210810119    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: c6b7f2de3c1940e6ab30986c0a49acc5            Boot ID: cc1b675076c44f709fc39742a2b5d6b8     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-48-generic       Architecture: x86-64  18:55:28 up 23:14,  1 user,  load average: 0.36, 0.47, 0.38

--- abstract-machine/klib/include/klib.h
+++ abstract-machine/klib/include/klib.h
@@ -37,7 +37,7 @@ int    sprintf   (char *str, const char *format, ...);
 int    snprintf  (char *str, size_t size, const char *format, ...);
 int    vsprintf  (char *str, const char *format, va_list ap);
 int    vsnprintf (char *str, size_t size, const char *format, va_list ap);
-int dig2str(char *str, int dig);
+//int dig2str(char *str, int dig);
 
 // assert.h
 #ifdef NDEBUG
