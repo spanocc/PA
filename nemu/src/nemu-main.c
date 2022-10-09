b0b5a64b5df459f6554e14b93a358555663750e3
tracer-ics2021 <tracer@njuics.org>
2022-10-09 09:15:21 -0700

    >  compile 210810119    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: c6b7f2de3c1940e6ab30986c0a49acc5            Boot ID: cc1b675076c44f709fc39742a2b5d6b8     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-48-generic       Architecture: x86-64  09:15:21 up 1 day, 11:43,  1 user,  load average: 1.37, 1.74, 1.34

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -13,10 +13,10 @@ int main(int argc, char *argv[]) {
   am_init_monitor();
 #else
 //打印参数
-  /*printf("argc:%d\n",argc);
+  printf("argc:%d\n",argc);
   for(int i = 0; i < argc; ++i) {
       printf("argv[%d]:%s\n", i, argv[i]);
-  }*/
+  }
 
   init_monitor(argc, argv);
 #endif
