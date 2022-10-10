f7153f4dc96806a9421c458288ffbdd8626a6fc9
tracer-ics2021 <tracer@njuics.org>
2022-10-09 22:04:24 -0700

    >  compile 210810119    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: c6b7f2de3c1940e6ab30986c0a49acc5            Boot ID: cc1b675076c44f709fc39742a2b5d6b8     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-48-generic       Architecture: x86-64  22:04:24 up 1 day, 12:03,  1 user,  load average: 0.11, 0.18, 0.49

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -77,7 +77,7 @@ void init_ftrace() {
 //printf("aaaaaaaaaaaaaaaaaaaaaaaa\n");
 
 
-    for(int i = 0; i < elf_head.e_shnum; i++) {          printf("%d\n",i);
+    for(int i = 0; i < elf_head.e_shnum; i++) {          printf("%d %s\n",i,shstrtab+shdr[i].sh_name);
         if(!strcmp(shstrtab+shdr[i].sh_name, ".symtab")) {      //printf("111\n777\n");
             rewind(fp);
             fseek(fp, shdr[i].sh_offset, SEEK_SET);
