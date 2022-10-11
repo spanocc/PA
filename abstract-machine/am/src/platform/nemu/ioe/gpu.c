ab86e65787c15c38bd2250e48a079b55ba023f86
tracer-ics2021 <tracer@njuics.org>
2022-10-11 00:58:24 -0700

    >  compile 210810119    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: c6b7f2de3c1940e6ab30986c0a49acc5            Boot ID: cc1b675076c44f709fc39742a2b5d6b8     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-48-generic       Architecture: x86-64  00:58:24 up 2 days,  2:33,  1 user,  load average: 0.24, 0.19, 0.18

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,12 +1,15 @@
 #include <am.h>
 #include <nemu.h>
-
+#include<stdio.h>
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
   int i;
   int w = io_read(AM_GPU_CONFIG).width;  // TODO: get the correct width
   int h = io_read(AM_GPU_CONFIG).height ;  // TODO: get the correct height
+
+printf("%s:%d %d\n","__am_gpu_init",w,h);
+
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
   outl(SYNC_ADDR, 1);  //马上将帧缓冲中的内容同步到屏幕上
@@ -29,6 +32,9 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   int x = ctl->x;
   int y = ctl->y;
   int width = io_read(AM_GPU_CONFIG).width;
+
+printf("%s:%d\n","__am_gpu_fbdraw",width);
+  
   uint32_t *p = ctl->pixels;
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for(int i = 0; i < w; ++i) {
