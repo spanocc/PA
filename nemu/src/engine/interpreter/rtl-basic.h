54d0b9c24d5ce6df78884348bae0c6cd26e67351
tracer-ics2021 <tracer@njuics.org>
2022-10-11 08:30:26 -0700

    >  compile 210810119    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: c6b7f2de3c1940e6ab30986c0a49acc5            Boot ID: cc1b675076c44f709fc39742a2b5d6b8     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-48-generic       Architecture: x86-64  08:30:26 up 2 days,  8:28,  1 user,  load average: 0.25, 0.49, 0.35

--- nemu/src/engine/interpreter/rtl-basic.h
+++ nemu/src/engine/interpreter/rtl-basic.h
@@ -44,7 +44,7 @@ def_rtl_compute_reg_imm(sraw)
 #define rtl_sraiw rtl_srawi
 #endif
 
-static inline def_rtl(setrelop, uint32_t relop, rtlreg_t *dest,
+ def_rtl(setrelop, uint32_t relop, rtlreg_t *dest,
     const rtlreg_t *src1, const rtlreg_t *src2) {
   *dest = interpret_relop(relop, *src1, *src2);
 }
