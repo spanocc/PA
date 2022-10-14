87b35223f0059db2abbdbb76444bceae94c0d5b9
tracer-ics2021 <tracer@njuics.org>
2022-10-14 00:29:05 -0700

    >  compile 210810119    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: c6b7f2de3c1940e6ab30986c0a49acc5            Boot ID: 43a5c61e021945eea77d79f38e7e64a8     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-50-generic       Architecture: x86-64  00:29:05 up  5:05,  1 user,  load average: 0.34, 0.27, 0.22

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -8,6 +8,22 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {//  printf("checking.
         flag = false;
         printf("0x%x: pc fault!   spike: 0x%x nemu: 0x%x\n",pc, ref_r->pc, cpu.pc);
     }
+    if(ref_r->mepc != cpu.mepc) {
+        flag = false;
+        printf("0x%x: mepc fault!   spike: 0x%x nemu: 0x%x\n",pc, ref_r->mepc, cpu.mepc);
+    }
+    if(ref_r->mstatus != cpu.mstatus) {
+        flag = false;
+        printf("0x%x: mstatus fault!   spike: 0x%x nemu: 0x%x\n",pc, ref_r->mstatus, cpu.mstatus);
+    }
+    if(ref_r->mcause != cpu.mcause) {
+        flag = false;
+        printf("0x%x: mcause fault!   spike: 0x%x nemu: 0x%x\n",pc, ref_r->mcause, cpu.mcause);
+    }
+    if(ref_r->mtvec != cpu.mtvec) {
+        flag = false;
+        printf("0x%x: mtvec fault!   spike: 0x%x nemu: 0x%x\n", pc, ref_r->mtvec, cpu.mtvec);
+    }
     for(int i = 0; i < 32; ++i) {
         if(ref_r->gpr[i]._32 != cpu.gpr[i]._32) {
             flag = false;
