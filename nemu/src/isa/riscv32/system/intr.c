1091b0a1cd849a3a071f5b1e9130cc48dbd248ac
210810119-fish and bird <1223784387@qq.com>
2022-10-15 17:47:45 -0700

    navy-apps ics2021 initialized
    navy-apps 13ba3997899ca9fe83e4c4c98a498ea06d41cfdd use ssh to clone code from github

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -8,7 +8,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   cpu.mcause = NO;
   vaddr_t in_addr = cpu.mtvec;
 #ifdef CONFIG_ETRACE
-printf("exception trace:\nmepc: %d\nmcause: %d\nmtvec: %d\n",cpu.mepc,cpu.mcause,cpu.mtvec);
+printf("\nexception trace:\nmepc: %d\nmcause: %d\nmtvec: %d\n\n",cpu.mepc,cpu.mcause,cpu.mtvec);
 #endif
 
   return in_addr;
