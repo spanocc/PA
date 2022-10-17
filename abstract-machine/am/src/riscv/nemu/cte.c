4c24666f62d6651386b58d46986742ab9ca4554c
tracer-ics2021 <tracer@njuics.org>
2022-10-17 08:44:14 -0700

    >  compile 210810119    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: c6b7f2de3c1940e6ab30986c0a49acc5            Boot ID: dd7deb7120324b7f8e0f69e4b3d11146     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-50-generic       Architecture: x86-64  08:44:15 up 23:38,  1 user,  load average: 0.55, 0.37, 0.35

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -14,7 +14,7 @@ Context* __am_irq_handle(Context *c) {  //这个c指针是trap.s汇编代码中�
     switch (c->mcause) {
       case 11:     //$a7 是第一个参数，也就是syscall函数中的type参数，且type从0开始（enum类型）
         printf("mcause == 11\n");
-        printf("$a7 == %d\n\n", c->gpr[17]);        printf("%d %x %d\n\n", c->gpr[0],c->gpr[1],c->gpr[2]);
+        printf("$a7 == %d\n\n", c->gpr[17]);        printf("%d %x %d\n\n", (int)(c->gpr[0]),c->gpr[1],c->gpr[2]);
         if(c->gpr[17] == -1) {
           ev.event = EVENT_YIELD; 
           c->mepc += 4;
