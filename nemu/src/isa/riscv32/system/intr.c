90ae3b9e9a3e672abdb810a14ae1fb679961b6ba
tracer-ics2021 <tracer@njuics.org>
2022-10-17 01:05:19 -0700

    >  compile 210810119    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: c6b7f2de3c1940e6ab30986c0a49acc5            Boot ID: dd7deb7120324b7f8e0f69e4b3d11146     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-50-generic       Architecture: x86-64  01:05:19 up 16:00,  1 user,  load average: 0.57, 0.56, 0.49

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -8,7 +8,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   cpu.mcause = NO;
   vaddr_t in_addr = cpu.mtvec;
 #ifdef CONFIG_ETRACE
-printf("\nexception trace:\nmepc: %d\nmcause: %d\nmtvec: %d\n\n",cpu.mepc,cpu.mcause,cpu.mtvec);
+printf("\nexception trace:\nmepc: 0x%x\nmcause: %d\nmtvec: 0x%x\n\n",cpu.mepc,cpu.mcause,cpu.mtvec);
 #endif
 
   return in_addr;
