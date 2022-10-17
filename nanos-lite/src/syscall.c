649f5872bd63676254a1147d2e5d805fffec4c9b
tracer-ics2021 <tracer@njuics.org>
2022-10-17 08:06:22 -0700

    >  compile 210810119    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: c6b7f2de3c1940e6ab30986c0a49acc5            Boot ID: dd7deb7120324b7f8e0f69e4b3d11146     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-50-generic       Architecture: x86-64  08:06:22 up 23:01,  1 user,  load average: 0.46, 0.38, 0.29

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -5,10 +5,15 @@
 
 uintptr_t sys_yield();
 void sys_exit(uintptr_t);
+uintptr_t sys_write(int fd, const void * buf, uintptr_t count);
+
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
   switch (a[0]) {
     case SYS_yield:
       c->GPRx = sys_yield();
@@ -21,11 +26,14 @@ void do_syscall(Context *c) {
     case SYS_exit:
 
       #ifdef CONFIG_STRACE
-        printf("sys_exit(%d) == void\n", a[0]);
+        printf("sys_exit(%d) == void\n", a[1]);
       #endif
 
       sys_exit(a[0]);
       break;
+    case SYS_write:
+      c->GPRx = sys_write(a[1], (void *)a[2], a[3]);
+
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
@@ -38,4 +46,15 @@ uintptr_t sys_yield() {
 
 void sys_exit(uintptr_t _exit) {
   halt(_exit);
+}
+
+uintptr_t sys_write(int fd, const void * buf, uintptr_t count) {
+  if(fd == 1 || fd == 2) {
+    for(uintptr_t i = 0; i < count; ++i) {
+      const char *c = buf;
+      putch(*(c + i));
+    }
+    return count;
+  }
+  return 0;
 }
