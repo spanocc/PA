edf6baeef6fbedc1171d12f2c3f73e740c955401
tracer-ics2021 <tracer@njuics.org>
2022-10-17 00:04:32 -0700

    >  compile 210810119    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: c6b7f2de3c1940e6ab30986c0a49acc5            Boot ID: dd7deb7120324b7f8e0f69e4b3d11146     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-50-generic       Architecture: x86-64  00:04:32 up 14:59,  1 user,  load average: 1.02, 0.60, 0.41

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -37,14 +37,17 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   assert(elf_head.e_machine == EXPECT_TYPE);
 
 // Elf_Phdr *phar = malloc(sizeof(Elf_Phdr) * elf_head.e_phnum);
-  Elf_Phdr *phar = (Elf_Phdr *)(&ramdisk_start + elf_head.e_phoff);    printf("\n\n%p\n\n", phar);
+// Elf_Phdr *phar = (Elf_Phdr *)(&ramdisk_start + elf_head.e_phoff);   // printf("\n\n%p\n\n", phar);
+  Elf_Phdr phar;
   for(int i = 0; i < elf_head.e_phnum; ++i) {  //printf("%d",i);
-    if(phar[i].p_type == PT_LOAD) {      printf("%x\n", phar[i].p_vaddr);
-      const char *buf = (char *)(&ramdisk_start + phar[i].p_offset);
+    ramdisk_read(&phar, elf_head.e_phoff + i * sizeof(phar), sizeof(phar)); //必须借助memcpy函数把phdr结构体读进来，直接用指针访问的话会有地址不是4字节对齐的问题，所以只要是访问内存都用memcpy函数就行了，而创建的这个phdr变量肯定是对齐的
+    //if(phar[i].p_type == PT_LOAD) {      这样子访问p_type会产生lw操作，且地址不是4字节对齐，会有difftest错误                       //printf("%x\n", phar[i].p_vaddr);
+    if(phar.p_type == PT_LOAD) {
+      const char *buf = (char *)(&ramdisk_start + phar.p_offset);
       //size_t offest = (uint8_t *)phar[i].p_vaddr - &ramdisk_start;
       //ramdisk_write(buf, offest, phar[i].p_filesz);
-      memcpy((uint8_t *)phar[i].p_vaddr, buf, phar[i].p_filesz);
-      memset((char *)(phar[i].p_vaddr + phar[i].p_filesz), 0, phar[i].p_memsz - phar[i].p_filesz);
+      memcpy((uint8_t *)phar.p_vaddr, buf, phar.p_filesz);
+      memset((char *)(phar.p_vaddr + phar.p_filesz), 0, phar.p_memsz - phar.p_filesz);
     }
   }
   return elf_head.e_entry;
