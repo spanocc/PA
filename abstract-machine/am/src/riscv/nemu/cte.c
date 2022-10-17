15c3802d76afbb841dce344478c673da09f12a80
tracer-ics2021 <tracer@njuics.org>
2022-10-17 02:11:10 -0700

    >  compile 210810119    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: c6b7f2de3c1940e6ab30986c0a49acc5            Boot ID: dd7deb7120324b7f8e0f69e4b3d11146     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-50-generic       Architecture: x86-64  02:11:10 up 17:05,  1 user,  load average: 0.82, 0.38, 0.36

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -12,10 +12,14 @@ Context* __am_irq_handle(Context *c) {  //这个c指针是trap.s汇编代码中�
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      /*case 11: 
-        ev.event = EVENT_YIELD; 
-        c->mepc += 4;
-        break;*/
+      case 11: 
+        printf("mcause == 11\n");
+        printf("$a7 == %d\n", c->gpr[17]);
+        if(c->gpr[17] == -1) {
+          ev.event = EVENT_YIELD; 
+          c->mepc += 4;
+          break;
+        }
       default: ev.event = EVENT_ERROR; break;
     }
 
