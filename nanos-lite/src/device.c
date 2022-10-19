69cb6cb368a0ce534e1e76b74f8f6dfbf6f01bb6
tracer-ics2021 <tracer@njuics.org>
2022-10-19 06:58:58 -0700

    >  compile 210810119    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: c6b7f2de3c1940e6ab30986c0a49acc5            Boot ID: dd7deb7120324b7f8e0f69e4b3d11146     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-50-generic       Architecture: x86-64  06:58:58 up 1 day, 15:53,  1 user,  load average: 0.67, 0.76, 0.62

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -22,14 +22,14 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {  //printf("\n\
   return len;
 }
 
-size_t events_read(void *buf, size_t offset, size_t len) {  printf("\n\n\nssssssssssssss\n\n\n");
+size_t events_read(void *buf, size_t offset, size_t len) {  //printf("\n\n\nssssssssssssss\n\n\n");
   size_t ret = 0;
   char event[32] = "ku ";
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
   if(ev.keycode == AM_KEY_NONE) return 0;
   if(ev.keydown) event[1] = 'd';
   strcat(event, keyname[ev.keycode]);
-  strcat(event, "\n");
+  strcat(event, "\n");                                printf("\n\n\n%s\n\n\n",event);
   while(event[ret] != '\0' && ret < len) {
     *(char *)buf = event[ret++];
   } //空字符不复制
