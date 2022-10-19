b5e15a714afc7916f3b24296d4aaab5070343c0d
tracer-ics2021 <tracer@njuics.org>
2022-10-19 03:52:45 -0700

    >  compile 210810119    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: c6b7f2de3c1940e6ab30986c0a49acc5            Boot ID: dd7deb7120324b7f8e0f69e4b3d11146     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-50-generic       Architecture: x86-64  03:52:45 up 1 day, 14:16,  1 user,  load average: 0.48, 0.52, 0.51

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -2,10 +2,10 @@
 #include<NDL.h>
 
 int main() {
-    uint32_t msec = 1000;
+    uint32_t msec = 500;
     while(1) {
         while(NDL_GetTicks() < msec);
         printf("%dms\n",msec);
-        msec += 1000;
+        msec += 500;
     }
 }
