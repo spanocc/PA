c1394c8c94a374a10e5037e73cdeaf87444a53b0
tracer-ics2021 <tracer@njuics.org>
2022-10-20 06:47:54 -0700

    >  compile 210810119    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: c6b7f2de3c1940e6ab30986c0a49acc5            Boot ID: 2f859aaab55d4bd7bde558aa5cebdc26     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-52-generic       Architecture: x86-64  06:47:54 up 12:29,  1 user,  load average: 0.54, 0.70, 0.71

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -20,15 +20,18 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
     dx = (int)(dstrect->x);
     dy = (int)(dstrect->y);
   }
+
+   printf("fill: %d %d %d %d\n %d %d %d %d\n",sx,sy,sw,sh,dx,dy,dw,dh);
+
   uint32_t *sp = (uint32_t *)(src->pixels);
   uint32_t *dp = (uint32_t *)(dst->pixels);
   for(int i = 0; i < sh; ++i) {
       for(int j = 0; j < sw; ++j) {
-           dp[(dy+i) * dst->w + dx+j] = sp[(sy+i) * src->w + sx+j];
+           dp[(dy+i) * (dst->w) + dx+j] = sp[(sy+i) * (src->w) + sx+j];
       }
   }
-
-  NDL_DrawRect((uint32_t *)dst->pixels, dx, dy, dw, dh);//将s画布的矩形写到屏幕上
+  
+  //NDL_DrawRect((uint32_t *)dst->pixels, dx, dy, dw, dh);//将s画布的矩形写到屏幕上
 }
 
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
@@ -39,14 +42,15 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
     tw = (int)(dstrect->w);
     th = (int)(dstrect->h);
   }
+  printf("fill: %d %d %d %d\n",x,y,tw,th);
   uint32_t *p = (uint32_t *)(dst->pixels);
   for(int i = 0; i < th; ++i) {
       for(int j = 0; j < tw; ++j) {
-           p[(y+i) * dst->w + x+j] = color;
+           p[(y+i) * (dst->w) + x+j] = color;
       }
   }
 
-  NDL_DrawRect((uint32_t *)dst->pixels, x, y, tw, th);//将s画布的矩形写到屏幕上NDL_DrawRect((uint32_t *)s->pixels, x, y, tw, th);//将s画布的矩形写到屏幕上
+  //NDL_DrawRect((uint32_t *)dst->pixels, x, y, tw, th);//将s画布的矩形写到屏幕上NDL_DrawRect((uint32_t *)s->pixels, x, y, tw, th);//将s画布的矩形写到屏幕上
 }
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
