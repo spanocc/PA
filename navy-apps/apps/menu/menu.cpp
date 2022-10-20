2069dad5c0c68fdb46214993f4c2cd89f5d866bb
tracer-ics2021 <tracer@njuics.org>
2022-10-20 07:15:50 -0700

    >  compile 210810119    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: c6b7f2de3c1940e6ab30986c0a49acc5            Boot ID: 2f859aaab55d4bd7bde558aa5cebdc26     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-52-generic       Architecture: x86-64  07:15:50 up 12:57,  1 user,  load average: 1.64, 1.16, 0.83

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -144,7 +144,7 @@ static void display_menu(int n) {
   SDL_Rect rect = { .x = screen->w - logo_sf->w, .y = 0 };
   SDL_BlitSurface(logo_sf, NULL, screen, &rect);
 
-SDL_UpdateRect(screen, 0, 0, 0, 0);
+SDL_UpdateRect(screen, 0, 0, 0, 0);  assert(0);
 
   printf("Available applications:\n");
   char buf[80];
