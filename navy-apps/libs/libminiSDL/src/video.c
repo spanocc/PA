0939a9c3f46aed715191e1aeee894a4b2b9e5125
tracer-ics2021 <tracer@njuics.org>
2022-10-20 02:16:57 -0700

    >  compile 210810119    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: c6b7f2de3c1940e6ab30986c0a49acc5            Boot ID: 2f859aaab55d4bd7bde558aa5cebdc26     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-52-generic       Architecture: x86-64  02:16:57 up  7:58,  1 user,  load average: 0.45, 0.43, 0.42

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect) {
   assert(dst && src);
@@ -13,6 +14,7 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 }
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
+  printf("%d\n%d\n%d\n%d\n",w,h,s->w,s->h);
   NDL_DrawRect((uint32_t *)s->pixels, x, y, w, h);
 }
 
