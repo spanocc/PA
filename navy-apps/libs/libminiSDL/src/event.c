814a22527d8d63b4c5eb10cc72e8d5ff81288498
tracer-ics2021 <tracer@njuics.org>
2022-10-20 07:57:19 -0700

    >  compile 210810119    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: c6b7f2de3c1940e6ab30986c0a49acc5            Boot ID: 2f859aaab55d4bd7bde558aa5cebdc26     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-52-generic       Architecture: x86-64  07:57:19 up 13:39,  1 user,  load average: 0.75, 0.52, 0.39

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -16,7 +16,18 @@ int SDL_PushEvent(SDL_Event *ev) {
 }
 
 int SDL_PollEvent(SDL_Event *ev) {
-  return 0;
+  char buf[32];
+  int ret = NDL_PollEvent(buf, sizeof(buf));
+  //while(!(NDL_PollEvent(buf, sizeof(buf))));   //printf("%s\n",buf);
+  if(!ret) return 0;
+  ev->type = (buf[1] == 'd') ? SDL_KEYDOWN : SDL_KEYUP; 
+  for(int i = 0; i < KEY_NUM; ++i) {  //printf("%s\n",keyname[i]);
+    if(!strncmp(buf+3, keyname[i], strlen(buf+3)-1)) {  //buf的最后有个换行符，不能比较    
+      ev->key.keysym.sym = i;
+      break;
+    }
+  } 
+  return 1;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
