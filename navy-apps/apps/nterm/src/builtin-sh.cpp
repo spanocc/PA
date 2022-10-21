766b9ac3dd718d5a2df18793395ebc71232818d0
tracer-ics2021 <tracer@njuics.org>
2022-10-21 06:04:28 -0700

    >  compile 210810119    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: c6b7f2de3c1940e6ab30986c0a49acc5            Boot ID: 2f859aaab55d4bd7bde558aa5cebdc26     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-52-generic       Architecture: x86-64  06:04:28 up 1 day,  2:05,  1 user,  load average: 0.80, 1.01, 0.91

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -26,7 +26,7 @@ static void sh_handle_cmd(const char *cmd) {
   char *buf = (char *)malloc(strlen(buf));
   strcpy(buf, cmd);
   buf[strlen(buf) - 1] = '\0';  printf("%s\n",buf);
-  execve(cmd, NULL, NULL);
+  //execve(cmd, NULL, NULL);
 }
 
 void builtin_sh_run() {
