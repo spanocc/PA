596e5b302f2f8a586c49df6212e670f95930df32
tracer-ics2021 <tracer@njuics.org>
2022-10-22 09:00:25 -0700

    >  compile 210810119    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: c6b7f2de3c1940e6ab30986c0a49acc5            Boot ID: 2f859aaab55d4bd7bde558aa5cebdc26     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-52-generic       Architecture: x86-64  09:00:25 up 1 day,  7:42,  1 user,  load average: 0.44, 0.36, 0.33

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -68,7 +68,7 @@ int printf(const char *fmt, ...) {
     ret = vsprintf(buf, fmt, ap);
     va_end(ap);
 
-    assert(ret <= 655360);
+    assert(ret <= 65536);
 
     buf[ret] = '\0';
     char *p = buf;
