46ab2ae302ddb9ab97e42b3e5edeb37b54ab2279
tracer-ics2021 <tracer@njuics.org>
2022-10-25 08:30:21 -0700

    >  compile 210810119    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: c6b7f2de3c1940e6ab30986c0a49acc5            Boot ID: 7b8b371d8f50433c8e4a208680538158     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-52-generic       Architecture: x86-64  08:30:21 up 18:31,  1 user,  load average: 0.68, 0.56, 0.53

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -12,12 +12,12 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {  //printf("S:%x\n"
   page = (((paddr_t)vaddr) >> 12) & 0x3ff; // printf("xia:%d %d\n");
 
   paddr_t dir_p, page_p;
-  dir_p = ((cpu.satp) << 12) + dir * 4;  printf("dir:%x\n",dir_p);
-  uint32_t pte = paddr_read(dir_p, 1);
+  dir_p = ((cpu.satp) << 12) + dir * 4;  //printf("dir:%x\n",dir_p);
+  uint32_t pte = paddr_read(dir_p, 4);
   assert((pte & 1) == 1);
 
   page_p = (pte & (~0xfff)) + page * 4;
-  pte = paddr_read(page_p, 1);
+  pte = paddr_read(page_p, 4);
   assert((pte & 1) == 1);
 
   paddr_t ret = ((pte) & (~0xfff)) | MEM_RET_OK;         printf("%d\n",ret);
