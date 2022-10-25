1071a3feba249719b3b2d6adee67ad60924abc9c
tracer-ics2021 <tracer@njuics.org>
2022-10-25 06:53:45 -0700

    >  compile 210810119    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: c6b7f2de3c1940e6ab30986c0a49acc5            Boot ID: 7b8b371d8f50433c8e4a208680538158     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-52-generic       Architecture: x86-64  06:53:45 up 16:54,  1 user,  load average: 0.53, 0.41, 0.32

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -3,11 +3,11 @@
 
 word_t vaddr_ifetch(vaddr_t addr, int len) {
 
-  if(isa_mmu_check(addr, len, MEM_TYPE_IFETCH)) {
+  if(isa_mmu_check(addr, len, MEM_TYPE_IFETCH) == MMU_DIRECT) {
     paddr_t pa = isa_mmu_translate(addr, len, MEM_TYPE_IFETCH);
     pa |= (addr & 0xfff);
     assert(pa == addr); 
-    paddr_read(pa, len);
+    return paddr_read(pa, len);
   }
 
   return paddr_read(addr, len);
@@ -15,11 +15,11 @@ word_t vaddr_ifetch(vaddr_t addr, int len) {
 
 word_t vaddr_read(vaddr_t addr, int len) {
 
-  if(isa_mmu_check(addr, len, MEM_TYPE_READ)) {
+  if(isa_mmu_check(addr, len, MEM_TYPE_READ) == MMU_DIRECT) {
     paddr_t pa = isa_mmu_translate(addr, len, MEM_TYPE_READ);
     pa |= (addr & 0xfff);
     assert(pa == addr); 
-    paddr_read(pa, len);
+    return paddr_read(pa, len);
   }
 
   return paddr_read(addr, len);
@@ -27,11 +27,12 @@ word_t vaddr_read(vaddr_t addr, int len) {
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
 
-  if(isa_mmu_check(addr, len, MEM_TYPE_WRITE)) {
+  if(isa_mmu_check(addr, len, MEM_TYPE_WRITE) == MMU_DIRECT) {
     paddr_t pa = isa_mmu_translate(addr, len, MEM_TYPE_WRITE);
     pa |= (addr & 0xfff);
     assert(pa == addr); 
     paddr_write(pa, len, data);
+    return;
   }
 
   paddr_write(addr, len, data);
