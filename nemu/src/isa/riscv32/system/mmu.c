2488ae79020c177b01acf79b6cb173a5a2c40a24
tracer-ics2021 <tracer@njuics.org>
2022-10-25 08:18:56 -0700

    >  compile 210810119    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: c6b7f2de3c1940e6ab30986c0a49acc5            Boot ID: 7b8b371d8f50433c8e4a208680538158     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-52-generic       Architecture: x86-64  08:18:56 up 18:19,  1 user,  load average: 0.38, 0.46, 0.50

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -5,14 +5,14 @@
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {  //printf("S:%x\n",cpu.satp);
   //assert(cpu.satp >> 31);
 
-  
+  printf("%x\n",vaddr);
 
   paddr_t dir, page;
   dir = (((paddr_t)vaddr) >> 22) & 0x3ff; 
   page = (((paddr_t)vaddr) >> 12) & 0x3ff;
 
   paddr_t dir_p, page_p;
-  dir_p = ((cpu.satp) << 12) + dir * 4;
+  dir_p = ((cpu.satp) << 12) + dir * 4;  
   uint32_t pte = paddr_read(dir_p, 1);
   assert((pte & 1) == 1);
 
