48d9cf9650e6a60bcdaf7a88199d54cf53810f49
tracer-ics2021 <tracer@njuics.org>
2022-10-26 05:53:27 -0700

    >  compile 210810119    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: c6b7f2de3c1940e6ab30986c0a49acc5            Boot ID: 7b8b371d8f50433c8e4a208680538158     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-52-generic       Architecture: x86-64  05:53:27 up 1 day,  3:22,  1 user,  load average: 0.55, 0.48, 0.41

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -5,15 +5,15 @@
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {  //printf("S:%x\n",cpu.satp);
   //assert(cpu.satp >> 31);
 
- //if(vaddr < 0x80000000) 
- printf("%x\n",vaddr);
+// if(vaddr < 0x80000000) 
+// printf("%x\n",vaddr);
 
   paddr_t dir, page;
   dir = (((paddr_t)vaddr) >> 22) & 0x3ff; 
   page = (((paddr_t)vaddr) >> 12) & 0x3ff; // printf("xia:%d %d\n");
 
   paddr_t dir_p, page_p;
-  dir_p = ((cpu.satp) << 12) + dir * 4;  //printf("dir:%x\n",dir_p);
+  dir_p = ((cpu.satp) << 12) + dir * 4;  if(vaddr == 0x4000523c) printf("dir:%x\n",dir_p);
   uint32_t pte = paddr_read(dir_p, 4);    //printf("%p\n",)
 
 //if((pte & 1) == 0) printf("ADD:%x %x\n",(cpu.satp<<12),dir_p); 
