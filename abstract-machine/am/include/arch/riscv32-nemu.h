9b10f5bc11519e92d987b61d76300b0106c40f66
tracer-ics2021 <tracer@njuics.org>
2022-10-26 09:11:42 -0700

    >  compile 210810119    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: c6b7f2de3c1940e6ab30986c0a49acc5            Boot ID: 7b8b371d8f50433c8e4a208680538158     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-52-generic       Architecture: x86-64  09:11:42 up 1 day,  6:40,  1 user,  load average: 0.68, 0.61, 0.50

--- abstract-machine/am/include/arch/riscv32-nemu.h
+++ abstract-machine/am/include/arch/riscv32-nemu.h
@@ -5,8 +5,12 @@
 
 struct Context {
   // TODO: fix the order of these members to match trap.S
-  uintptr_t gpr[32], mcause, mstatus, mepc;
-  void *pdir;
+  union {
+    struct {
+      uintptr_t gpr[32], mcause, mstatus, mepc;
+    };
+    void *pdir;
+  };
 };
 
 #define GPR1 gpr[17] // a7
