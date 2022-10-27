0bf9f46d4b09320b78024d41635b4757ed43c35c
tracer-ics2021 <tracer@njuics.org>
2022-10-26 19:44:18 -0700

    >  compile 210810119    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: c6b7f2de3c1940e6ab30986c0a49acc5            Boot ID: 7b8b371d8f50433c8e4a208680538158     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-52-generic       Architecture: x86-64  19:44:18 up 1 day,  9:09,  1 user,  load average: 0.93, 0.66, 0.45

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -68,13 +68,13 @@ static void clear_display(void) {
 }
 
 int main(int argc, char *argv[], char *envp[]) {           
-  SDL_Init(0);                                          assert(0);
-  screen = SDL_SetVideoMode(0, 0, 32, SDL_HWSURFACE);   assert(0);
+  SDL_Init(0);                                          
+  screen = SDL_SetVideoMode(0, 0, 32, SDL_HWSURFACE);
 
-  font = new BDF_Font(font_fname);           assert(0);
-  logo_sf = SDL_LoadBMP("/share/pictures/projectn.bmp");     assert(0);
+  font = new BDF_Font(font_fname);
+  logo_sf = SDL_LoadBMP("/share/pictures/projectn.bmp");
   assert(logo_sf);
-  set_i_max();          assert(0);
+  set_i_max();
                                              //assert(0);
   while (1) {
     display_menu(i_max);
