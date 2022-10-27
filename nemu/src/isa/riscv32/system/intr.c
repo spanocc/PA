814c44d1f923d8e7a17ca7c3d2729a3e9dd5b000
tracer-ics2021 <tracer@njuics.org>
2022-10-27 05:27:23 -0700

    >  compile 210810119    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: c6b7f2de3c1940e6ab30986c0a49acc5            Boot ID: 2ca3a7c4c29d4db8b1ea98550598326a     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-52-generic       Architecture: x86-64  05:27:23 up  7:27,  1 user,  load average: 0.45, 0.39, 0.44

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -24,7 +24,7 @@ printf("\nexception trace:\nmepc: 0x%x\nmcause: %d\nmtvec: 0x%x\n\n",cpu.mepc,cp
 }
 
 word_t isa_query_intr() {
-  if (((cpu.mstatus >> 3) & 1) && cpu.INTR == true) {
+  if (((cpu.mstatus >> 3) & 1) && cpu.INTR == true) {   printf("b\n");
     cpu.INTR = false;
     return IRQ_TIMER;
   }
